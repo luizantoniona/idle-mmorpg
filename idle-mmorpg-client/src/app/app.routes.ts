e533385414b7e53d6590da5e0310943eef875f42
luizantoniona <luizantonio.na@hotmail.com>
2025-08-02 19:05:37 -0300

    fix: adjust routes

--- idle-mmorpg-client/src/app/app.routes.ts
+++ idle-mmorpg-client/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginPage } from './pages/login/login.page';
+
 import { AccountPage } from './pages/account/account.page';
+import { GamePage } from './pages/game/game.page';
+import { LoginPage } from './pages/login/login.page';
 
 export const routes: Routes = [
     { path: '', component: LoginPage },
     { path: 'account', component: AccountPage },
+    { path: 'game', component: GamePage },
 ];
