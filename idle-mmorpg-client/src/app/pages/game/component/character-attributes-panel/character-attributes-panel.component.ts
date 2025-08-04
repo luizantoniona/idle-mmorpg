512036ce23ee8c747bd99fb13b3e929d9a717358
luizantoniona <luizantonio.na@hotmail.com>
2025-08-04 17:28:26 -0300

    feat: Adjust attributes component to use panel vertical

--- idle-mmorpg-client/src/app/pages/game/component/character-attributes-panel/character-attributes-panel.component.ts
+++ idle-mmorpg-client/src/app/pages/game/component/character-attributes-panel/character-attributes-panel.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
 
+import { PanelVerticalComponent } from '../../../../component';
+
 import { Character } from '../../../../model';
 
 @Component({
     selector: 'app-character-attributes-panel',
     templateUrl: './character-attributes-panel.component.html',
     styleUrl: './character-attributes-panel.component.scss',
-    imports: [],
+    imports: [
+        PanelVerticalComponent,
+    ],
 })
 
 export class CharacterAttributesPanel {
