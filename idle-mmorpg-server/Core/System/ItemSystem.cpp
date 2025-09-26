ac27f2448fdd131270dbd0c4d4a0164909bb55d0
luizantoniona <luizantonio.na@hotmail.com>
2025-09-26 01:31:58 -0300

    fix: Notify skill bonuses

--- idle-mmorpg-server/Core/System/ItemSystem.cpp
+++ idle-mmorpg-server/Core/System/ItemSystem.cpp
@@ -194,6 +194,7 @@ void ItemSystem::computeEquipmentModifiers( const std::string& sessionId, Model:
     }
 
     NotificationSystem::notifyCharacterAttributes( sessionId, character );
+    NotificationSystem::notifyCharacterSkills( sessionId, character );
 }
 
 } // namespace Core::System
