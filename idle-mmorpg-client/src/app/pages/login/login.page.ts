595eee915ea2a9a76d3974759a1fcda3e87eabc0
luizantoniona <luizantonio.na@hotmail.com>
2025-10-22 21:50:12 -0300

    fix: minor update on quotes and login.page

--- idle-mmorpg-client/src/app/pages/login/login.page.ts
+++ idle-mmorpg-client/src/app/pages/login/login.page.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import { ServerFormComponent } from './component/server-form/server-form.component';
-import { LoginFormComponent } from './component/login-form/login-form.component';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { ServerFormComponent } from "./component/server-form/server-form.component";
+import { LoginFormComponent } from "./component/login-form/login-form.component";
 
 @Component({
-    selector: 'app-login-page',
-    standalone: true,
+    selector: "app-login-page",
+    templateUrl: "./login.page.html",
+    styleUrls: ["./login.page.scss"],
     imports: [
         CommonModule,
         ServerFormComponent,
         LoginFormComponent,
     ],
-    templateUrl: './login.page.html',
-    styleUrls: ['./login.page.scss']
 })
+
 export class LoginPage { }
