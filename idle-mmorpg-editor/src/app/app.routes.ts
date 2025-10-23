0ed3778285dbd83a1e77d314a01cd590c9852211
luizantoniona <luizantonio.na@hotmail.com>
2025-10-22 22:17:21 -0300

    feat: Starting data editor project

---
+++ idle-mmorpg-editor/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+
+import { EditorPage } from "./editor/editor.page";
+
+export const routes: Routes = [
+    { path: "", component: EditorPage },
+];
---
+++ idle-mmorpg-editor/src/app/editor/editor.page.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: "app-editor-page",
+    templateUrl: "./editor.page.html",
+    styleUrls: ["./editor.page.scss"],
+    imports: [
+        CommonModule,
+    ],
+})
+
+export class EditorPage { }
