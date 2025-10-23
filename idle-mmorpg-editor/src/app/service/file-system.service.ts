bd9313b7f0c64ea23a5c014e6c75a55497e6d10a
luizantoniona <luizantonio.na@hotmail.com>
2025-10-23 17:25:37 -0300

    fix: Adjusting editor plugins

---
+++ idle-mmorpg-editor/src/app/service/file-system.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from "@angular/core";
+import { readDir, BaseDirectory } from "@tauri-apps/plugin-fs";
+
+@Injectable({ providedIn: "root" })
+export class FileSystemService {
+
+}
