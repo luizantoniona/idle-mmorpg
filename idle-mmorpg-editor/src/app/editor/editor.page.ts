c3a17fd7072942b13587fe12f7fa5f1a013ae647
luizantoniona <luizantonio.na@hotmail.com>
2025-10-28 22:32:41 -0300

    feat: starting the editor layout

--- idle-mmorpg-editor/src/app/editor/editor.page.ts
+++ idle-mmorpg-editor/src/app/editor/editor.page.ts
@@ -1,5 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { BaseDirectory } from "@tauri-apps/plugin-fs";
+
+import { FileSystemService } from "../service/file-system.service";
+
+import { FilePanelComponent } from "./component/file-panel.component";
 
 @Component({
     selector: "app-editor-page",
@@ -7,7 +12,19 @@ import { CommonModule } from "@angular/common";
     styleUrls: ["./editor.page.scss"],
     imports: [
         CommonModule,
+        FilePanelComponent,
     ],
 })
 
-export class EditorPage { }
+export class EditorPage implements OnInit {
+
+    constructor(private fsService: FileSystemService) { }
+
+    async ngOnInit() {
+        const files = await this.fsService.readDirectory("", BaseDirectory.Home);
+
+        files.forEach(f => {
+            console.log(f.name, f.isDirectory ? "(pasta)" : "(arquivo)");
+        });
+    }
+}
--- idle-mmorpg-editor/src/app/service/file-system.service.ts
+++ idle-mmorpg-editor/src/app/service/file-system.service.ts
@@ -4,4 +4,16 @@ import { readDir, BaseDirectory } from "@tauri-apps/plugin-fs";
 @Injectable({ providedIn: "root" })
 export class FileSystemService {
 
+    constructor() { }
+
+    async readDirectory(path: string = "", base: BaseDirectory = BaseDirectory.Home) {
+        try {
+            const entries = await readDir(path, { baseDir: base });
+            return entries;
+
+        } catch (error) {
+            console.error("Error", error);
+            return [];
+        }
+    }
 }
