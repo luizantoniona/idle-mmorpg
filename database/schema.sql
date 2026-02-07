6903bce5b5ecec4f54a8b384bebcd32c5e8ea187
luizantoniona <luizantonio.na@hotmail.com>
2026-02-07 17:38:20 -0300

    fix: remove id_stage from character table

--- database/schema.sql
+++ database/schema.sql
@@ -7,7 +7,6 @@ CREATE TABLE user (
 CREATE TABLE 'character' (
     id_character INTEGER PRIMARY KEY AUTOINCREMENT,
     id_user INTEGER NOT NULL,
-    id_stage INTEGER NOT NULL,
     ds_name VARCHAR(20) NOT NULL,
 
     FOREIGN KEY (id_user) REFERENCES user(id_user) ON DELETE CASCADE
